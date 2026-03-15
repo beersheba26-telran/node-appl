b6082f2c2ab3b124c5b5f9946034663c338a1e78
Yuragranovsky <yuragranovsky@gmail.com>
2026-03-15 15:31:14 +0200

    summing two numbers from command line

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,40 @@
 import "dotenv/config"
 import logger from "./logger.js"
-logger.debug("HELLO WORLD")
+function getParameters(): number[] {
+    const firstNumberStr = process.argv[2];
+    const firstNumber = processArgument(firstNumberStr, "first")
+    logger.debug(`firstNumber is ${firstNumber}`)
+    const secondNumberStr = process.argv[3];
+    const secondNumber = processArgument(secondNumberStr, "second")
+    logger.debug(`secondNumber is ${secondNumber}`)
+   
+    return [firstNumber, secondNumber]
+} 
+function sum(firstNumber: number, secondNumber: number): number {
+    logger.debug(`function sum arguments : ${firstNumber} and ${secondNumber}`)
+    return firstNumber + secondNumber
+}
+function main() {
+   try {
+     const numbers: number[] = getParameters()
+     logger.debug(`parameters of main are ${numbers}`)
+     const result: number = sum(numbers[0] || 0, numbers[1] || 0)
+     console.log(`sum of ${numbers[0]} and ${numbers[1]} is ${result}`)
+ }
+    catch (error) {
+        const errorMessage = error as Error
+        logger.error(errorMessage.message)
+   }
+}
+function processArgument(param: string | undefined, seqNum: string): number {
+    if(!param) {
+        throw Error(seqNum +  " number must be provided in arguments")
+    }
+    const num = Number(param);
+    
+    if (isNaN(num)) {
+        throw Error(seqNum + " argument must be number")
+    }
+    return num
+}
+main();
