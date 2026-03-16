19cd59f520267025e7b380dd7edf4dd5fa03eef5
Yuragranovsky <yuragranovsky@gmail.com>
2026-03-16 13:37:11 +0200

    write to file

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,9 @@
 import "dotenv/config"
 import os from "node:os"
 import logger from "./logger.js"
-import fs from "node:fs/promises"
-const main = async () => {logger.info(`project directory is ${process.cwd()}`)
-const content = await fs.readFile(process.cwd() + "/src/logger.ts", {encoding: "ascii"})
-console.log(content) //printing whole file
-const lines: string[] = content.split('\n')
-logger.info(`first line of the file is ${lines[0]}`)}
-main()
+import fs, { writeFile } from "node:fs"
+const content: string = "שלום עולם"
+fs.writeFileSync("file.txt", content, {encoding: "utf8"})
+const readContent = fs.readFileSync("file.txt", {encoding: "utf8"})
+console.log(Array.from(readContent).reverse().join(""))
+
