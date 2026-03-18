7db8b70bf2c49ee6cceba53bed0be7f6e30555a2
Yuragranovsky <yuragranovsky@gmail.com>
2026-03-18 14:58:05 +0200

    backpressure time measuring for writing to file

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,33 @@
 import "dotenv/config";
 import logger from "./logger.js";
-<<<<<<< HEAD
-import LoggerEmitter from "./LoggerEmitter.js";
-import ConsoleHandler from "./consoleLogHandler.js";
-import FileHandler from "./fileLogHandler.js";
-import TimeFormatter from "./TimeFormatter.js";
-import errorHandler from "./ErrorLogHandler.js";
-const formatter =  new TimeFormatter("EST");
-const loggerEmitter = new LoggerEmitter([new ConsoleHandler(formatter), new FileHandler("logs.txt", formatter)]);
-const messages: string[] = ["Hello world", "What's up", "How are you", "Bye"]
-loggerEmitter.setLevelHandler("error", errorHandler)
-messages.forEach(m => loggerEmitter.log("info",m))
-loggerEmitter.log("error", "some error 3")
-const errors: string[] = errorHandler.getErrors(e => parseInt(e.substring(e.indexOf(":") + 1)) > 40);
-logger.info(errors.length == 0 ? "No errors" : errors)
-=======
-import { createReadStream, ReadStream } from "node:fs";
-let countChunk: number = 0
-const kbBytes = 1024 
-const stream: ReadStream = createReadStream("large_file", {encoding: "binary", highWaterMark: 1024 * 1024 * 500});
-console.time("running time of reading");
-(async () => {
-  for await (const chunk of stream) {
-     countChunk++;
-  logger.debug(`chunk with ${chunk.length/kbBytes} Kb`)
+import { createWriteStream } from "node:fs";
+const writeStream = createWriteStream("large_file", {encoding: "utf8", highWaterMark: 1024 * 1024 * 10})
+const nChunks = 1_000_00
+const chunkNumbers = 1000
+ const numbers: string = getRandomNumbers(chunkNumbers).join(" ");
+let i = 0;
+function writeNumbers() {
+  let canWrite = true;
+  
+  while (canWrite && i < nChunks) {
+    canWrite = writeStream.write(numbers)
+    i++;
   }
-  console.timeEnd("running time of reading")
-      console.log("count of chunks is ", countChunk)
-}) ()
+  if (i < nChunks) {
+      writeStream.once("drain",writeNumbers) //waiting for draining internal buffer
+  } else {
+     writeStream.end()
+  }
+  
+ 
+}
+function getRandomNumbers(chunkNumbers: number): number[] {
+  return Array.from ({length: chunkNumbers}, () => Math.trunc(Math.random() * 1000))
+}
+console.time("write");
+writeStream.on("finish", () => {console.timeEnd("write"); })
 
-
->>>>>>> 7b6e5d4 (ReadStream from large file)
-
+writeNumbers()
 
 
 
