fc453ee203acf45f1457e8794c2f5bddd1d19010
Yuragranovsky <yuragranovsky@gmail.com>
2026-03-19 13:34:23 +0200

    code before break

--- src/RandomNumbersStream.ts
+++ src/RandomNumbersStream.ts
@@ -1,17 +1,32 @@
 
 import { Readable, ReadableOptions } from "node:stream";
 export default class RandomNumbersStream extends Readable {
-    counter: number = 0
-    constructor(private _amount: number, options: ReadableOptions={encoding: "utf-8"}) {
+    private _counter: number = 0
+    constructor(private _amount: number, private _min: number, private _max: number,
+        private _chunkNumbers: number = 1024 * 1024,
+         options: ReadableOptions={}) {
         super(options)
+        if (_min > _max) {
+            [_min, _max] = [_max, _min]
+        }
     }
     override _read(_: number): void {
-        if (this.counter >= this._amount) {
+        if (this._counter >= this._amount) {
             this.push(null)
         } else {
-            this.push(this.counter + " ")
-            this.counter++
+            const chunkNumbers = Math.min(this._amount - this._counter, this._chunkNumbers)
+            const buffer: Buffer = this._getChunkBuffer(chunkNumbers);
+            this.push(buffer)
+            this._counter += chunkNumbers
         }
 
+    }
+    private _getChunkBuffer(count: number) : Buffer {
+       const array =  new Uint32Array(count)
+       for (let i = 0; i < count; i++){
+        array[i] = this._min + Math.trunc(Math.random() * (this._max - this._min + 1))
+       }
+       const resBuffer = Buffer.from(array.buffer)
+       return resBuffer
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,9 @@ import "dotenv/config";
 import logger from "./logger.js";
 import { createWriteStream } from "node:fs";
 import RandomNumbersStream from "./RandomNumbersStream.js";
-const writeStream = createWriteStream("large_file", {encoding: "utf8", highWaterMark: 1024 * 1024 * 10})
+const writeStream = createWriteStream("large_file", { highWaterMark: 1024 * 1024 * 10})
 console.time("pipe")
-new RandomNumbersStream(1_000_000_000).pipe(writeStream)
+new RandomNumbersStream(1_000_000_000, 10, 3000000).pipe(writeStream)
 writeStream.on("finish", ()=>console.timeEnd("pipe"))
 
 
