d8eb4fade193c00c02f063b4e066c066a2397306
Yuragranovsky <yuragranovsky@gmail.com>
2026-03-22 16:17:45 +0200

    POST Zod validation and error handling with task definition in README.md

---
+++ README.md
@@ -0,0 +1,4 @@
+# Zod validation and handling of errors
+## Update code of index.ts
+1. Introduce Zod validation see https://www.npmjs.com/package/zod <br>
+2. Update errors handling taking into consideration Zod validation
--- src/controller/index.ts
+++ src/controller/index.ts
@@ -1,6 +1,6 @@
 import "dotenv/config";
 import logger from "../logger.js";
-import http, { ServerResponse } from "node:http";
+import http, { ServerResponse, IncomingMessage } from "node:http";
 import calculate from "../service/calculate.js";
 import { CalcData } from "../model/CalcData.js";
 import { ServiceError } from "../errors/ServiceError.js";
@@ -8,12 +8,12 @@ const server = http.createServer();
 const port = process.env.PORT || 3500;
 server.listen(port, () => console.log(`server listening on port ${port}`));
 
-server.on("request", (req, res) => {
+server.on("request", async (req, res) => {
   logger.debug(`URL is ${req.url}`);
   try {
-    const calcData = getCalcData(req.url);
+    const calcData = await getCalcData(req);
     logger.debug("calcData is " + JSON.stringify(calcData));
-      const result = calculate(calcData).toString();
+      const result =  calculate(calcData).toString();
       logger.debug(`result = ${result}`);
       sendResponse(res, 200, result)
     }
@@ -26,18 +26,13 @@ server.on("request", (req, res) => {
   }
 
 });
-function getCalcData(url: string | undefined): CalcData {
-  const urlTokens: any = url?.split("/");
-  if (!url || urlTokens.length != 4)
-    throw new ServiceError(400, "Wrong request, usage /<operation>/<op1>/<op2>");
-  let result: CalcData | null = null;
-  const op1: number = +urlTokens[2];
-  const op2: number = +urlTokens[3];
-  const operation: string = urlTokens[1];
-  if (isNaN(op1)) throw new ServiceError(400, `an operand op1 (${urlTokens[2]}) must be a number`);
-  if (isNaN(op2)) throw new ServiceError(400, `an operand op1 (${urlTokens[3]}) must be a number`);
-  result = { op1, op2, operation };
-
+async function getCalcData(request: IncomingMessage): Promise<CalcData> {
+   let data = ""
+    for await (const chunk of request){
+        data += chunk
+    }
+    logger.debug(`received JSON is ${data}`)
+    const result: CalcData = JSON.parse(data)
   return result;
 }
 function sendResponse(response: ServerResponse, code: number, message: string): void {
